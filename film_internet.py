28d62f652729dee1d7ce620d52b12f26302d3de8
Sophren314 <sophren314@gmail.com>
2026-05-02 20:40:42 +0200

    Update film_internet.py

--- film_internet.py
+++ film_internet.py
@@ -15,7 +15,7 @@ from gspread_dataframe import set_with_dataframe
 sl.markdown("""
     <style>
     .stApp {
-        background-image: url("");
+        background-image: url("https://github.com/Sophren314/selecteur-de-film/blob/5181754157c3a2f1e9fbe74affa56b07ba078399/IMG_0144.jpeg");
         background-size: cover;
         background-position: center;
     }
