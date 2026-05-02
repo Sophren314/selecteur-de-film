56bcb449c7e101c9d55e6ff4542cc91eb7909a79
Sophren314 <sophren314@gmail.com>
2026-05-02 20:54:14 +0200

    Update film_internet.py

--- film_internet.py
+++ film_internet.py
@@ -12,10 +12,6 @@ import gspread as gs
 import streamlit as sl
 from gspread_dataframe import set_with_dataframe
 
-import url('https://fonts.googleapis.com/css2?family=NOM_DE_LA_POLICE&display=swap');
-* {
-    font-family: 'Cinzel', serif;
-}
 
 sl.markdown("""
     <style>
