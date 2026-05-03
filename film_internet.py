90372c590c81560b933f7362cd15ee52c9601332
Sophren314 <sophren314@gmail.com>
2026-05-03 10:02:55 +0200

    Update film_internet.py

--- film_internet.py
+++ film_internet.py
@@ -133,7 +133,7 @@ graph = pd.DataFrame({
 fig = px.bar(graph, x='Personne', y='Films vus', title='Films vus par personne',
             color_discrete_sequence = ['#FFFFFF','FF80FF'])
 fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-                 width=300, height=400)
+                 width=300, height=400, marker_line_width=2)
 sl.plotly_chart(fig)
 
 
