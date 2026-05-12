e2323712afb9acd61adab733e3bd387bdbc6af52
Sophren314 <sophren314@gmail.com>
2026-05-12 10:05:54 +0200

    Update film_internet.py

--- film_internet.py
+++ film_internet.py
@@ -146,7 +146,7 @@ angela_vu = len(film[film['Angela'] == 'X'])
 
 rang1,rang2,rang3 = sl.columns(3)
 with rang1:
-    sl.metric(label='Angela il te reste :' , value = len(film[film['Angela'] == 'O']) , color='blue')
+    sl.metric(label='Angela il te reste :' , value = len(film[film['Angela'] == 'O']))
 with rang2:
     sl.metric(label='il reste au total :' , value = len(films))
 with rang3:
